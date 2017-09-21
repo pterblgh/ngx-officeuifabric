72341a0d5b13cc4289ecb3070ed2a10e12ac8bd3
pterblgh <pterblgh@gmail.com>
2017-09-22 00:10:38 +0200

    Export all newly created helper

--- tools/gulp/helpers/index.ts
+++ tools/gulp/helpers/index.ts
@@ -1,1 +1,5 @@
 export * from './run-sequence';
+export * from './create-copy-task';
+export * from './create-watch-task';
+export * from './create-clean-task';
+export * from './create-compile-task';

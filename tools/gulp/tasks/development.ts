56db21bfe3778afbb2a57ea8b1742200fe7104ab
pterblgh <pterblgh@gmail.com>
2017-09-22 00:12:40 +0200

    Use task factories from gulp-helpers instead of hand-defined tasks

--- tools/gulp/tasks/development.ts
+++ tools/gulp/tasks/development.ts
@@ -1,42 +1,21 @@
 import { resolve } from 'path';
 import { task, watch, src, dest } from 'gulp';
-import * as del from 'del';
-import * as trypescript from 'gulp-typescript';
 import * as gulpSass from 'gulp-sass';
 import { server, reload } from 'gulp-connect';
-import { runSequence } from 'gulp-helpers';
+import { runSequence, createCopyTask, createWatchTask, createCleanTask, createCompileTask } from 'gulp-helpers';
 import { buildConfig } from 'build';
 
 const demoApp = resolve(buildConfig.srcDir, 'demo-app');
 
 const tscConfig = require(resolve(demoApp, 'tsconfig.json'));
 
-task('clean', () => del('dist/**/*'));
-
-task('copy:assets', () => {
-    const assets = ['index.html', 'systemjs.config.js', 'systemjs-angular-loader.js'];
-    return src(assets.map(asset => `${demoApp}/${asset}`))
-        .pipe(dest('dist/demo-app'));
-});
-
-task('copy:vendor', () => {
-    const vendor = [
-        'node_modules/core-js/client/shim.min.js',
-        'node_modules/zone.js/dist/zone.js',
-        'node_modules/systemjs/dist/system.src.js',
-        'node_modules/@angular/**/*.js',
-        'node_modules/rxjs/**/*.js',
-    ];
-    return src(vendor, { base: './node_modules' })
-        .pipe(dest('dist/demo-app/node_modules'));
-});
-
-task('compile:ts', () => {
-    return src('src/demo-app/**/*.ts')
-    .pipe(trypescript(tscConfig.compilerOptions))
-    .pipe(dest('dist/demo-app'))
-    .pipe(reload());
-});
+const vendor = [
+    'node_modules/core-js/client/shim.min.js',
+    'node_modules/zone.js/dist/zone.js',
+    'node_modules/systemjs/dist/system.src.js',
+    'node_modules/@angular/**/*.js',
+    'node_modules/rxjs/**/*.js',
+];
 
 task('connect', () => {
     server({
@@ -52,25 +31,34 @@ task('sass', () => {
         .pipe(reload());
 });
 
-task('watch:ts', () => {
-    watch('src/demo-app/**/*.ts', ['compile:ts']);
-});
+const compileTask = createCompileTask('demo-app:ts', demoApp, 'dist/demo-app', tscConfig.compilerOptions);
 
-task('watch:sass', () => {
-    watch('src/demo-app/**/*.scss', ['sass']);
-});
+const watchers = [
+    {
+        name: 'demo-app:ts',
+        path: `${demoApp}/**/*.ts`,
+        tasks: [compileTask]
+    },
+    {
+        name: 'demo-app:sass',
+        path: `${demoApp}/**/*.scss`,
+        tasks: ['sass']
+    }
+];
+
+const watchTask = watchers.map(watch => createWatchTask(watch.name, watch.path, watch.tasks));
 
 task('build', runSequence(
-    'clean', 
-    'copy:assets', 
-    'copy:vendor', 
-    'compile:ts',
-    'sass'
+    createCleanTask('demo-app', 'dist/demo-app'),
+    createCopyTask('demo-app:assets', ['**/*.html', '**/*.js'], { path: demoApp }, 'dist/demo-app'),    
+    createCopyTask('demo-app:vendor', vendor, { path: buildConfig.projectDir, base: './node_modules' }, 'dist/demo-app/node_modules'),    
+    compileTask,
+    'sass',
+    ...watchTask
 ));
 
 task('development', runSequence(
     'build',
-    'watch:ts',
-    'watch:sass',
+    ...watchTask,
     'connect'
 ));

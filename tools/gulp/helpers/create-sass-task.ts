7142bbc3c7d37c5eed74cb31e60a5462fbf500c6
pterblgh <pterblgh@gmail.com>
2017-09-26 23:34:21 +0200

    Use proper substring for comparison

--- tools/gulp/helpers/create-sass-task.ts
+++ tools/gulp/helpers/create-sass-task.ts
@@ -10,7 +10,7 @@ import * as gulpSass from 'gulp-sass';
 export function createSassTask(taskName: string, path: string, outPath: string): string {
     taskName = `sass:${taskName}`;
     const scssGlob = '/**/*.scss';
-    if (path.slice(scssGlob.length) !== scssGlob) {
+    if (path.slice(-scssGlob.length) !== scssGlob) {
         path += scssGlob;
     }
     task(taskName, () => {

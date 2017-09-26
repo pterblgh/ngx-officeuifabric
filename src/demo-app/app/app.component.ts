b64591c1f68c48b6c7bfad4382f14b56f4e96128
pterblgh <pterblgh@gmail.com>
2017-09-26 23:32:04 +0200

    Use FbButtonComponent in demo-app’s AppComponent

--- src/demo-app/app/app.component.ts
+++ src/demo-app/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1><fb-button></fb-button>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  { name = 'ngx-officeuifabric!'; }
--- src/demo-app/app/app.module.ts
+++ src/demo-app/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FbButtonModule } from 'ngx-fabric';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, FbButtonModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })

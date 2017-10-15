b726af5334b6ce3a7b13c6c056f113aa679cf0c5
pterblgh <pterblgh@gmail.com>
2017-10-15 23:28:37 +0200

    feat(form-element): create basic form-element component

---
+++ src/lib/form-element/form-element.component.ts
@@ -0,0 +1,24 @@
+import { Component, ContentChild, AfterContentInit } from '@angular/core';
+import { FabricInputDirective } from './input.directive';
+import { FabricLabelDirective } from './label.directive';
+
+@Component({
+    selector: 'fab-form-element',
+    templateUrl: './form-element.component.html',
+    styleUrls: [ './form-element.component.css' ],
+    host: {
+        'class': 'fab-form-container',
+    }
+})
+export class FabricFormElementComponent implements AfterContentInit {
+
+    @ContentChild(FabricLabelDirective) private _labelElement: FabricLabelDirective;
+    @ContentChild(FabricInputDirective) private _inputElement: FabricInputDirective;
+
+    ngAfterContentInit() {
+        if (!this._inputElement) {
+            console.error('input element not declared');
+        }
+    }
+
+}
---
+++ src/lib/form-element/label.directive.ts
@@ -0,0 +1,6 @@
+import { Directive } from "@angular/core";
+
+@Directive({
+    selector: '[fab-label]'
+})
+export class FabricLabelDirective { }

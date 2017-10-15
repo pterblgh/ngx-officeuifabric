59adbbcd6d2bce7f3da2b44cb4ba7fd98a6139a4
pterblgh <pterblgh@gmail.com>
2017-10-15 23:29:14 +0200

    feat(form-element): use form-element component to wrap native input fields

--- src/demo-app/app/app.module.ts
+++ src/demo-app/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FabricButtonModule, FabricCompoundButtonModule, FabricCheckboxModule } from 'ngx-fabric';
+import { FabricButtonModule, FabricCompoundButtonModule, FabricCheckboxModule, FabricFormElementModule } from 'ngx-fabric';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule, FabricButtonModule, FabricCompoundButtonModule, FabricCheckboxModule ],
+  imports: [
+    BrowserModule,
+    FabricButtonModule,
+    FabricCompoundButtonModule,
+    FabricCheckboxModule,
+    FabricFormElementModule
+  ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })

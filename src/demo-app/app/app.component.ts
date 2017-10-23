184824e4fd3c3d4d8de769f5ab051653b4d7402a
pterblgh <pterblgh@gmail.com>
2017-10-23 18:54:18 +0200

    ref: add ngModel to input element

--- src/demo-app/app/app.component.ts
+++ src/demo-app/app/app.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  { name = 'ngx-officeuifabric!'; }
+export class AppComponent {
+
+  inputField: string;
+
+}
--- src/demo-app/app/app.module.ts
+++ src/demo-app/app/app.module.ts
@@ -1,19 +1,23 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FabricButtonModule, FabricCompoundButtonModule, FabricCheckboxModule, FabricFormElementModule, FabricTextfieldModule } from 'ngx-fabric';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
+    CommonModule,
     BrowserModule,
+    FormsModule,
     FabricButtonModule,
     FabricCompoundButtonModule,
     FabricCheckboxModule,
     FabricFormElementModule,
     FabricTextfieldModule
   ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }

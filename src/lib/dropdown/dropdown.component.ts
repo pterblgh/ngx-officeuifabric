c8cc10ce266307ca56ee294ca94ea97382fd8a73
pterblgh <pterblgh@gmail.com>
2017-11-18 11:12:32 +0100

    feat(dropdown): define inputs and outputs

--- src/lib/dropdown/dropdown.component.ts
+++ src/lib/dropdown/dropdown.component.ts
@@ -1,8 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { DropdownItem } from './dropdown-item.interface';
 
 @Component({})
 export class FabricDropdownComponent implements OnInit {
 
+    @Input() placeholder: string;
+    @Input() label: string;
+    @Input() options: DropdownItem[];
+
+    @Output() itemSelected: EventEmitter<DropdownItem>;
+
+    constructor() {
+        this.itemSelected = new EventEmitter();
+    }
 
     ngOnInit(): void {
         console.log('FabricDropdownComponent initialized');

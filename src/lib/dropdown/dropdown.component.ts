04cd11f1e46b3a945ea75986727c795082ca6da2
pterblgh <pterblgh@gmail.com>
2017-11-20 19:17:03 +0100

    feat(dropdown): implement dropdown without animation

--- src/lib/dropdown/dropdown.component.ts
+++ src/lib/dropdown/dropdown.component.ts
@@ -1,25 +1,63 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DropdownItem } from './dropdown-item.interface';
+import { DropdownItemType } from './dropdown-item-type.enum';
 
 @Component({
     selector: 'fab-dropdown',
     templateUrl: './dropdown.component.html',
-    styleUrls: ['./dropdown.component.css']
+    styleUrls: ['./dropdown.component.css'],
+    host: {
+        '[class.disabled]': 'disabled'
+    }
 })
 export class FabricDropdownComponent implements OnInit {
 
     @Input() placeholder: string;
     @Input() label: string;
-    @Input() options: DropdownItem[];
+    @Input() items: DropdownItem[];
+    @Input() disabled: boolean;
+    @Input() defaultSelectedKey: string;
 
     @Output() itemSelected: EventEmitter<DropdownItem>;
+
+    DropdownItemType = DropdownItemType;
+    isItemContainerVisible = false;
+    _placeholder: string;
+    _currentSelectedItem: DropdownItem;
+
+    set currentSelectedItem(item: DropdownItem | null) {
+        if (item) {
+            this._currentSelectedItem = item;
+            this._placeholder = item.text;
+            this.isItemContainerVisible = false;
+            this.itemSelected.emit(item);
+        }
+    }
+
+    get currentSelectedItem() {
+        return this._currentSelectedItem;
+    }
 
     constructor() {
         this.itemSelected = new EventEmitter();
     }
 
     ngOnInit(): void {
-        console.log('FabricDropdownComponent initialized');
+        this._placeholder = this.placeholder;
+        if (this.defaultSelectedKey) {
+            const item = this.items.find(item => item.key === this.defaultSelectedKey);
+            this.currentSelectedItem = item || null;
+        }
+    }
+
+    onPlaceholderClicked(_event: MouseEvent) {
+        this.isItemContainerVisible = !this.isItemContainerVisible;
+    }
+
+    selectItem(item: DropdownItem) {
+        if (!item.itemType) {
+            this.currentSelectedItem = item;
+        }
     }
 
 }

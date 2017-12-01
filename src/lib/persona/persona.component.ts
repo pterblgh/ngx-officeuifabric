225310241204a97583548a443a128a36a51db398
pterblgh <pterblgh@gmail.com>
2017-12-01 12:48:51 +0100

    ref(persona): move calculateInitials() method to a common PersonaService

--- src/lib/persona/persona.component.ts
+++ src/lib/persona/persona.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { PersonaSize } from './persona-size.enum';
 import { PersonaInitialsColor } from './persona-initials-colors.enum';
 import { PersonaPresence } from './persona-presence.enum';
+import { FabricPersonaService } from './persona.service';
 
 @Component({
   selector: 'fab-persona',
@@ -37,55 +38,15 @@ export class FabricPersonaComponent implements OnInit {
     this._size = value;
   }
 
+  constructor(private readonly personaService: FabricPersonaService) { }
+
   ngOnInit(): void {
     this.realSize = Number(this.size.split('size')[1]);
-    if (!this.imageUrl) {
-      const initials = this.calculateInitials();
+
+    if (!this.imageUrl && this.primaryText) {
+      const initials = this.personaService.calculateInitials(this.primaryText);
       this.imageInitials = this.imageInitials || initials.imageInitials;
       this.initialsColor = this.initialsColor || initials.initialsColor;
     }
   }
-
-  private calculateInitials(): CalculatedInitials {
-    let imageInitials = '';
-    let initialsColor = PersonaInitialsColor.Blue;
-    const personaColors = [
-      PersonaInitialsColor.LightBlue,
-      PersonaInitialsColor.Blue,
-      PersonaInitialsColor.DarkBlue,
-      PersonaInitialsColor.Teal,
-      PersonaInitialsColor.LightGreen,
-      PersonaInitialsColor.Green,
-      PersonaInitialsColor.DarkGreen,
-      PersonaInitialsColor.Magenta,
-      PersonaInitialsColor.Purple,
-      PersonaInitialsColor.Black,
-      PersonaInitialsColor.Orange,
-      PersonaInitialsColor.Red,
-      PersonaInitialsColor.DarkRed
-    ];
-    if (this.primaryText) {
-      const splittedPrimaryText = this.primaryText.split(' ');
-      let value = 0;
-      const primaryTextLength = this.primaryText.length;
-
-      if (splittedPrimaryText.length > 1) {
-        imageInitials += splittedPrimaryText[0][0];
-        imageInitials += splittedPrimaryText[1][0];
-      }
-      for (let i = 0; i < primaryTextLength; i++) {
-        value += this.primaryText.charCodeAt(i);
-      }
-      initialsColor = personaColors[Math.floor(value % personaColors.length)];
-    }
-    return {
-      imageInitials,
-      initialsColor
-    };
-  }
-}
-
-interface CalculatedInitials {
-  imageInitials: string;
-  initialsColor: PersonaInitialsColor;
 }
---
+++ src/lib/persona/persona.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { PersonaInitialsColor } from './persona-initials-colors.enum';
+import { CalculatedInitials } from './calculated-initials.interface';
+
+@Injectable()
+export class FabricPersonaService {
+
+  private static _personaColors = [
+    PersonaInitialsColor.LightBlue,
+    PersonaInitialsColor.Blue,
+    PersonaInitialsColor.DarkBlue,
+    PersonaInitialsColor.Teal,
+    PersonaInitialsColor.LightGreen,
+    PersonaInitialsColor.Green,
+    PersonaInitialsColor.DarkGreen,
+    PersonaInitialsColor.Magenta,
+    PersonaInitialsColor.Purple,
+    PersonaInitialsColor.Black,
+    PersonaInitialsColor.Orange,
+    PersonaInitialsColor.Red,
+    PersonaInitialsColor.DarkRed
+  ];
+
+  calculateInitials(primaryText: string): CalculatedInitials {
+    const personaColors = FabricPersonaService._personaColors;
+    let imageInitials = '';
+    let initialsColor = PersonaInitialsColor.Blue;
+
+    if (primaryText) {
+      const splittedPrimaryText = primaryText.split(' ');
+      let value = 0;
+      const primaryTextLength = primaryText.length;
+
+      if (splittedPrimaryText.length > 1) {
+        imageInitials += splittedPrimaryText[0][0];
+        imageInitials += splittedPrimaryText[1][0];
+      }
+      for (let i = 0; i < primaryTextLength; i++) {
+        value += primaryText.charCodeAt(i);
+      }
+      initialsColor = personaColors[Math.floor(value % personaColors.length)];
+    }
+
+    return {
+      imageInitials,
+      initialsColor
+    };
+  }
+
+}

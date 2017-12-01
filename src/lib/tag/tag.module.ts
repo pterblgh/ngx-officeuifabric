e54e712676d12685e1e07abe2e0c2f7ea6e67c58
pterblgh <pterblgh@gmail.com>
2017-12-01 15:17:36 +0100

    feat(tag): declare and export component

--- src/lib/tag/tag.module.ts
+++ src/lib/tag/tag.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FabricTagComponent } from './tag.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [],
-  exports: []
+  declarations: [
+    FabricTagComponent
+  ],
+  exports: [
+    FabricTagComponent
+  ]
 })
 export class FabricTagModule { }

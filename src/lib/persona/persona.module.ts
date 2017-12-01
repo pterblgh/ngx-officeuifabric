cdf0d9aa297df26881fd751242bfde2603dec893
pterblgh <pterblgh@gmail.com>
2017-12-01 12:50:59 +0100

    style(persona): use single quotes instead of double quotes in imports

--- src/lib/persona/persona.module.ts
+++ src/lib/persona/persona.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FabricPersonaComponent } from "./persona.component";
-import { FabricPersonaPillComponent } from "./persona-pill.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FabricPersonaComponent } from './persona.component';
+import { FabricPersonaPillComponent } from './persona-pill.component';
+import { FabricPersonaService } from './persona.service';
 
 @NgModule({
   imports: [

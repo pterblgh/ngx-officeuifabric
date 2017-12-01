a48c8a3e60919c73374082c622eec81c4b09f978
pterblgh <pterblgh@gmail.com>
2017-12-01 12:55:46 +0100

    feat(persona-pill): add size as input

--- src/lib/persona/persona-pill.component.ts
+++ src/lib/persona/persona-pill.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PersonaPillSize } from './persona-pill-size.enum';
 
 @Component({
   selector: 'fab-persona-pill',
   templateUrl: './persona-pill.component.html',
-  styleUrls: ['./persona-pill.component.css']
+  styleUrls: ['./persona-pill.component.css'],
+  host: {
+    'class': 'size'
+  }
 })
 export class FabricPersonaPillComponent implements OnInit {
+
+  @Input() size: PersonaPillSize;
 
   ngOnInit(): void {
     console.log(`FabricPersonaPillComponent initialized`);

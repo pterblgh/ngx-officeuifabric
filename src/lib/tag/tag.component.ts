0e91f23cd0afea4d6355dde44cb661941a609aeb
pterblgh <pterblgh@gmail.com>
2017-12-01 21:49:15 +0100

    feat(tag): add inputs

--- src/lib/tag/tag.component.ts
+++ src/lib/tag/tag.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'fab-tag',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tag.component.css']
 })
 export class FabricTagComponent implements OnInit {
+
+  @Input() text: string;
+  @Output() deleteClicked = new EventEmitter<void>();
 
   ngOnInit(): void {
     console.log(`FabricTagComponent initialized`);

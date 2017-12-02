57c3199801daab085e2a8d7df836b1d47de44895
pterblgh <pterblgh@gmail.com>
2017-12-02 11:07:02 +0100

    feat(radio): add FabricRadioComponent to declarations and exports arrays

--- src/lib/radio/radio.module.ts
+++ src/lib/radio/radio.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FabricRadioComponent } from './radio.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [],
-  exports: []
+  declarations: [
+    FabricRadioComponent
+  ],
+  exports: [
+    FabricRadioComponent
+  ]
 })
 export class FabricRadioModule { }

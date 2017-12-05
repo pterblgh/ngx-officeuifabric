dc6f6240aa3bb8754ea57f54445ee753d0e2fa1d
pterblgh <pterblgh@gmail.com>
2017-12-05 23:04:26 +0100

    feat(callout): provide and export necessary elements

--- src/lib/callout/callout.module.ts
+++ src/lib/callout/callout.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FabricCalloutService } from './callout.service';
 import { FabricCalloutComponent } from './callout.component';
+import { FabricCalloutDirective } from './callout.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    FabricCalloutComponent
+    FabricCalloutComponent,
+    FabricCalloutDirective
   ],
   exports: [
-    FabricCalloutComponent
+    FabricCalloutComponent,
+    FabricCalloutDirective
   ],
+  entryComponents: [
+    FabricCalloutComponent
+  ]
 })
 export class FabricCalloutModule { }

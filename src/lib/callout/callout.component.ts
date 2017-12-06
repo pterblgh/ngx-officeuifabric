8cac23c442ac36d7af6413cc84e5afe2091004ac
pterblgh <pterblgh@gmail.com>
2017-12-06 20:25:05 +0100

    feat(callout): display data from injected config

--- src/lib/callout/callout.component.ts
+++ src/lib/callout/callout.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { FAB_CALLOUT_CONFIG } from './callout.tokens';
+import { CalloutConfig } from './callout-config.interface';
 
 @Component({
     selector: 'fab-callout',
@@ -7,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FabricCalloutComponent implements OnInit {
 
+    title: string;
+    content: string;
+
+    constructor(@Inject(FAB_CALLOUT_CONFIG) private _config: CalloutConfig) {}
+
     ngOnInit(): void {
-        console.log(`FabricCalloutComponent initialized`);
+        if (this._config) {
+            this.title = this._config.title;
+            this.content = this._config.content;
+        }
     }
 
 }

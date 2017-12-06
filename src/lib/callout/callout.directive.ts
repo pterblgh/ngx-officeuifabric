b80ca4174fcd84f7763c6dee95206ae6231d63a7
pterblgh <pterblgh@gmail.com>
2017-12-06 20:24:44 +0100

    feat(callout): implement config injecting into component opened via portal

--- src/lib/callout/callout.directive.ts
+++ src/lib/callout/callout.directive.ts
@@ -1,7 +1,11 @@
-import { Directive, OnInit, HostListener, ElementRef, OnDestroy } from '@angular/core';
+import { Directive, OnInit, HostListener, ElementRef, OnDestroy, Injector, Input } from '@angular/core';
 import { Overlay, OverlayRef, OverlayConfig, OriginConnectionPosition, OverlayConnectionPosition } from '@angular/cdk/overlay';
-import { Portal, ComponentPortal } from '@angular/cdk/portal';
+import { Portal, ComponentPortal, PortalInjector } from '@angular/cdk/portal';
 import { FabricCalloutComponent } from './callout.component';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { FAB_CALLOUT_CONFIG } from './callout.tokens';
+import { CalloutConfig } from './callout-config.interface';
 
 @Directive({
     selector: '[fab-callout], [fabCallout]'
@@ -9,32 +13,59 @@ import { FabricCalloutComponent } from './callout.component';
 export class FabricCalloutDirective implements OnDestroy {
 
     private _overlayRef: OverlayRef;
+    private _backdropClicked$: Observable<void>;
+    private _subscription: Subscription;
+    private _config: CalloutConfig;
+
+    @Input()
+    get calloutConfig(): CalloutConfig {
+        return this._config;
+    }
+    set calloutConfig(value: CalloutConfig) {
+        this._config = value;
+    }
 
     constructor(
+        private _injector: Injector,
         private _element: ElementRef,
         private _overlay: Overlay
     ) { }
 
     ngOnDestroy(): void {
         this._overlayRef.dispose();
+        this._subscription.unsubscribe();
     }
 
     @HostListener('click')
     private _openOverlay() {
-        const originPos: OriginConnectionPosition = { originX: 'end', originY: 'top' };
-        const overlayPos: OverlayConnectionPosition = { overlayX: 'start', overlayY: 'top' };
-        const config = new OverlayConfig({
-            positionStrategy: this._overlay.position().connectedTo(this._element, originPos, { overlayX: 'start', overlayY: 'top' }),
-            backdropClass: 'fab-callout-overlay-backdrop',
-            hasBackdrop: true,
-            maxWidth: 300
-        });
-        if (!this._overlayRef) {
-            this._overlayRef = this._overlay.create(config);
+        if (this._config) {
+            const originPos: OriginConnectionPosition = { originX: 'end', originY: 'top' };
+            const overlayPos: OverlayConnectionPosition = { overlayX: 'start', overlayY: 'top' };
+            const config = new OverlayConfig({
+                positionStrategy: this._overlay.position().connectedTo(this._element, originPos, overlayPos),
+                backdropClass: 'fab-callout-overlay-backdrop',
+                hasBackdrop: true,
+                maxWidth: 300
+            });
+            if (!this._overlayRef) {
+                this._overlayRef = this._overlay.create(config);
+                this._backdropClicked$ = this._overlayRef.backdropClick();
+                this._subscription = this._backdropClicked$.subscribe(() => this._overlayRef.detach());
+            }
+            const injector = this._createInjector(this._config);
+            const portal = new ComponentPortal(FabricCalloutComponent, null, injector);
+            this._overlayRef.attach(portal);
+        } else {
+            console.warn(`No config object provided for FabricCalloutDirective`);
         }
-        const portal: Portal<any> = new ComponentPortal(FabricCalloutComponent);
-        this._overlayRef.attach(portal);
-        this._overlayRef.backdropClick().subscribe(() => this._overlayRef.detach());
+    }
+
+    private _createInjector(config: CalloutConfig): PortalInjector {
+        const injectionTokens = new WeakMap();
+
+        injectionTokens.set(FAB_CALLOUT_CONFIG, config);
+
+        return new PortalInjector(this._injector, injectionTokens);
     }
 
 }

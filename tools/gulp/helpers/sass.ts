0fd7a1a2d4c9676a7da81988066e1ac98e80e063
pterblgh <pterblgh@gmail.com>
2018-02-24 15:49:56 +0100

    ref(gulp): always add Office UI Core sass variables to includePaths

--- tools/gulp/helpers/sass.ts
+++ tools/gulp/helpers/sass.ts
@@ -5,6 +5,8 @@ import * as autoprefixer from 'autoprefixer';
 /** gulp-postcss has no type definitions */
 const postcss = require('gulp-postcss');
 
+const OFFICE_UI_FABRIC_CORE = 'node_modules/office-ui-fabric-core/src/sass/variables';
+
 /**
  * Compiles SASS files to CSS
  * @param taskName
@@ -13,6 +15,7 @@ const postcss = require('gulp-postcss');
  * @param includePaths
  */
 export function sass(taskName: string, path: string, outPath: string, includePaths?: string[]): string {
+  includePaths = includePaths ? includePaths.concat(OFFICE_UI_FABRIC_CORE) : [OFFICE_UI_FABRIC_CORE];
   taskName = `sass:${taskName}`;
   const scssGlob = '/**/*.scss';
   if (path.slice(-scssGlob.length) !== scssGlob) {
@@ -20,7 +23,7 @@ export function sass(taskName: string, path: string, outPath: string, includePat
   }
   task(taskName, () => {
     return src(path)
-      .pipe(gulpSass(includePaths ? {includePaths} : undefined).on('error', gulpSass.logError))
+      .pipe(gulpSass(includePaths ? { includePaths } : undefined).on('error', gulpSass.logError))
       .pipe(postcss([autoprefixer()], { url: 'inline' }))
       .pipe(dest(outPath));
   });

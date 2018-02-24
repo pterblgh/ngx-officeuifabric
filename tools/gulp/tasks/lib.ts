7b7e56e1685bd0d21d44e5b9f0c1c1e2422e2f30
pterblgh <pterblgh@gmail.com>
2018-02-24 16:02:48 +0100

    build(lib): create ES2015 and ES5 build from lib

--- tools/gulp/tasks/lib.ts
+++ tools/gulp/tasks/lib.ts
@@ -1,51 +1,27 @@
-import { resolve, join } from 'path';
+import { join } from 'path';
 import { buildConfig } from 'build';
-import { createCompileTask, createSassTask, createCopyTask, runSequence, createWatchTask } from 'gulp-helpers';
+import { sass, copy, runSequence, inlineSources, ngc, clean } from 'gulp-helpers';
 import { task } from 'gulp';
 
-const libPath = resolve(buildConfig.srcDir, 'lib');
+const { srcDir, projectDir } = buildConfig;
 
-const tsConfigPath = require(resolve(join(libPath, 'tsconfig.json')));
-
-const LIB_OUT_PATH = 'dist/demo-app/lib';
-
-/**
- * Include all SASS definitions from office-ui-fabric-core
- * and from the common/styles library
- */
-const sassIncludePaths = [
-  'node_modules/office-ui-fabric-core/src/sass/variables',
-  `${libPath}/common/styles`,
-];
-
-const compileTask = createCompileTask('ngx-fabric:ts', libPath, LIB_OUT_PATH, tsConfigPath.compilerOptions);
-const sassTask = createSassTask('ngx-fabric', libPath, LIB_OUT_PATH, sassIncludePaths);
-const copyTask = createCopyTask('ngx-fabric:html', ['**/*.html'], {path: libPath}, LIB_OUT_PATH);
-
-const watchers = [
-  {
-    name: 'lib:ts',
-    path: `${libPath}/**/*.ts`,
-    tasks: [compileTask],
+const path = {
+  lib: join(srcDir, 'lib'),
+  outTsc: join(projectDir, 'out-tsc'),
+  temp: {
+    root: join(projectDir, 'temp'),
+    lib: join(projectDir, 'temp', 'lib'),
+    es2015: join(projectDir, 'temp', 'lib', 'tsconfig.lib.json'),
+    es5: join(projectDir, 'temp', 'lib', 'tsconfig.es5.json'),
   },
-  {
-    name: 'lib:sass',
-    path: `${libPath}/**/*.scss`,
-    tasks: [sassTask],
-  },
-  {
-    name: 'lib:html',
-    path: `${libPath}/**/*.html`,
-    tasks: [copyTask],
-  },
-];
-
-const watchTask = watchers.map(watch => createWatchTask(watch.name, watch.path, watch.tasks));
+};
 
 task('build:lib', runSequence(
-  compileTask,
-  sassTask,
-  copyTask,
+  clean('out-tsc', path.outTsc),
+  copy('temp', ['**/*'], { path: path.lib }, path.temp.lib),
+  sass('lib', path.temp.lib, path.temp.lib, [`${path.temp.lib}/common/styles`]),
+  inlineSources('lib', join(path.temp.lib, '**/*.component.ts'), path.temp.lib),
+  ngc('es2015', path.temp.es2015),
+  ngc('es5', path.temp.es5),
+  clean('temp', path.temp.root),
 ));
-
-task('watch:lib', runSequence(...watchTask));

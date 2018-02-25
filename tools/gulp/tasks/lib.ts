e883e2bed25bdcc2bd17a028d0eae8ede1e379aa
pterblgh <pterblgh@gmail.com>
2018-02-25 10:10:49 +0100

    ref(build): refactor inlineSources

--- tools/gulp/tasks/lib.ts
+++ tools/gulp/tasks/lib.ts
@@ -7,16 +7,11 @@ const { srcDir, projectDir } = buildConfig;
 
 const path = {
   lib: join(srcDir, 'lib'),
-  outTsc: {
-    root: join(projectDir, 'out-tsc'),
-    es2015: join(projectDir, 'out-tsc', 'lib-es2015'),
-    es5: join(projectDir, 'out-tsc', 'lib-es5'),
-  },
-  temp: {
-    root: join(projectDir, 'temp'),
-    lib: join(projectDir, 'temp', 'lib'),
-    es2015: join(projectDir, 'temp', 'lib', 'tsconfig.lib.json'),
-    es5: join(projectDir, 'temp', 'lib', 'tsconfig.es5.json'),
+  tmp: {
+    root: join(projectDir, 'tmp'),
+    src: join(projectDir, 'tmp', 'src'),
+    esm2015: join(projectDir, 'tmp', 'esm2015'),
+    esm5: join(projectDir, 'tmp', 'esm5'),
   },
   dist: {
     root: join(projectDir, 'dist'),
@@ -27,21 +22,27 @@ const path = {
   },
 };
 
+task('clean:sources', runSequence(
+  clean('tmp:html', `${path.tmp.src}/**/*.html`),
+  clean('tmp:sass', `${path.tmp.src}/**/*.scss`),
+  clean('tmp:css', `${path.tmp.src}/**/*.css`),
+));
+
 task('build:lib', runSequence(
-  clean('out-tsc', path.outTsc.root),
-  copy('temp', ['**/*'], { path: path.lib }, path.temp.lib),
-  sass('lib', path.temp.lib, path.temp.lib, [`${path.temp.lib}/common/styles`]),
-  inlineSources('lib', join(path.temp.lib, '**/*.component.ts'), path.temp.lib),
-  ngc('es5', path.temp.es5),
-  ngc('es2015', path.temp.es2015),
-  clean('temp', path.temp.root),
+  clean('tmp', path.tmp.root),
+  copy('tmp', ['**/*'], { path: path.lib }, path.tmp.src),
+  sass('lib', path.tmp.src, path.tmp.src, [`${path.tmp.src}/common/styles`]),
+  inlineSources('lib', join(path.tmp.src, '**/*.component.ts'), path.tmp.src),
+  'clean:sources',
+  ngc('esm5', join(path.tmp.src, 'tsconfig.esm5.json')),
+  ngc('esm2015', join(path.tmp.src, 'tsconfig.esm2015.json')),
   'bundle:lib',
 ));
 
 task('bundle:lib', runSequence(
   clean('dist', path.dist.root),
-  copy('metadata', ['**/*.metadata.json'], { path: path.outTsc.es2015 }, path.dist.root),
-  copy('type-definitions:lib', ['**/*.d.ts'], { path: path.outTsc.es2015 }, path.dist.root),
-  copy('esm5', ['**/*.js', '**/*.js.map'], { path: path.outTsc.es5 }, path.dist.esm5),
-  copy('esm2015', ['**/*.js', '**/*.js.map'], { path: path.outTsc.es2015 }, path.dist.esm2015),
+  copy('metadata', ['**/*.metadata.json'], { path: path.tmp.esm2015 }, path.dist.root),
+  copy('type-definitions:lib', ['**/*.d.ts'], { path: path.tmp.esm2015 }, path.dist.root),
+  copy('esm5', ['**/*.js', '**/*.js.map'], { path: path.tmp.esm5 }, path.dist.esm5),
+  copy('esm2015', ['**/*.js', '**/*.js.map'], { path: path.tmp.esm2015 }, path.dist.esm2015),
 ));

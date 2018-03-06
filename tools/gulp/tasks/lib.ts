bf1a12283af7ff6cac56866243cda6ab9b43335e
pterblgh <pterblgh@gmail.com>
2018-03-06 23:15:56 +0100

    build: implement library build

--- tools/gulp/tasks/lib.ts
+++ tools/gulp/tasks/lib.ts
@@ -1,6 +1,6 @@
 import { join } from 'path';
 import { buildConfig } from 'build';
-import { sass, copy, runSequence, inlineSources, ngc, clean } from 'gulp-helpers';
+import { sass, copy, runSequence, inlineSources, ngc, clean, rollup, RollupConfig, uglifyJsFile, htmlMin } from 'gulp-helpers';
 import { task } from 'gulp';
 
 const { srcDir, projectDir } = buildConfig;
@@ -18,7 +18,7 @@ const path = {
     src: join(projectDir, 'dist', 'src'),
     esm2015: join(projectDir, 'dist', 'esm2015'),
     esm5: join(projectDir, 'dist', 'esm5'),
-    bundle: join(projectDir, 'dist', 'bundle'),
+    bundle: join(projectDir, 'dist', 'bundles'),
   },
 };
 
@@ -31,6 +31,7 @@ task('clean:sources', runSequence(
 task('build:lib', runSequence(
   clean('tmp', path.tmp.root),
   copy('tmp', ['**/*'], { path: path.lib }, path.tmp.src),
+  htmlMin('tmp', path.tmp.src, path.tmp.src),
   sass('lib', path.tmp.src, path.tmp.src, [`${path.tmp.src}/common/styles`]),
   inlineSources('lib', join(path.tmp.src, '**/*.component.ts'), path.tmp.src),
   'clean:sources',
@@ -39,10 +40,91 @@ task('build:lib', runSequence(
   'bundle:lib',
 ));
 
+const rollupGlobals = {
+  'tslib': 'tslib',
+
+  '@angular/animations': 'ng.animations',
+  '@angular/core': 'ng.core',
+  '@angular/common': 'ng.common',
+  '@angular/forms': 'ng.forms',
+  '@angular/platform-browser': 'ng.platformBrowser',
+  '@angular/platform-browser-dynamic': 'ng.platformBrowserDynamic',
+  '@angular/cdk': 'ng.cdk',
+  '@angular/cdk/overlay': 'ng.cdk.overlay',
+  '@angular/cdk/portal': 'ng.cdk.portal',
+
+  'rxjs/BehaviorSubject': 'Rx',
+  'rxjs/Observable': 'Rx',
+  'rxjs/Subject': 'Rx',
+  'rxjs/Subscription': 'Rx',
+  'rxjs/Observer': 'Rx',
+  'rxjs/Subscriber': 'Rx',
+  'rxjs/Scheduler': 'Rx',
+
+  'rxjs/observable/combineLatest': 'Rx.Observable',
+  'rxjs/observable/forkJoin': 'Rx.Observable',
+  'rxjs/observable/fromEvent': 'Rx.Observable',
+  'rxjs/observable/merge': 'Rx.Observable',
+  'rxjs/observable/of': 'Rx.Observable',
+  'rxjs/observable/throw': 'Rx.Observable',
+  'rxjs/observable/defer': 'Rx.Observable',
+  'rxjs/observable/fromEventPattern': 'Rx.Observable',
+  'rxjs/observable/empty': 'Rx.Observable',
+
+  'rxjs/operators/debounceTime': 'Rx.operators',
+  'rxjs/operators/takeUntil': 'Rx.operators',
+  'rxjs/operators/take': 'Rx.operators',
+  'rxjs/operators/first': 'Rx.operators',
+  'rxjs/operators/filter': 'Rx.operators',
+  'rxjs/operators/map': 'Rx.operators',
+  'rxjs/operators/tap': 'Rx.operators',
+  'rxjs/operators/startWith': 'Rx.operators',
+  'rxjs/operators/auditTime': 'Rx.operators',
+  'rxjs/operators/switchMap': 'Rx.operators',
+  'rxjs/operators/finalize': 'Rx.operators',
+  'rxjs/operators/catchError': 'Rx.operators',
+  'rxjs/operators/share': 'Rx.operators',
+  'rxjs/operators/delay': 'Rx.operators',
+  'rxjs/operators/combineLatest': 'Rx.operators',
+};
+
+const esm2015RollupConfig: RollupConfig = {
+  input: join(path.tmp.esm2015, (require(join(path.tmp.src, 'tsconfig.esm2015.json'))).angularCompilerOptions.flatModuleOutFile),
+  external: Object.keys(rollupGlobals),
+  format: 'es',
+  output: {
+    file: join(path.dist.esm2015, (require(join(path.tmp.src, 'tsconfig.esm2015.json'))).angularCompilerOptions.flatModuleOutFile),
+  },
+};
+
+const esm5RollupConfig: RollupConfig = {
+  input: join(path.tmp.esm5, (require(join(path.tmp.src, 'tsconfig.esm5.json'))).angularCompilerOptions.flatModuleOutFile),
+  external: Object.keys(rollupGlobals),
+  format: 'es',
+  output: {
+    file: join(path.dist.esm5, (require(join(path.tmp.src, 'tsconfig.esm5.json'))).angularCompilerOptions.flatModuleOutFile),
+  },
+};
+
+const umdRollupConfig: RollupConfig = {
+  input: join(path.tmp.esm5, (require(join(path.tmp.src, 'tsconfig.esm5.json'))).angularCompilerOptions.flatModuleOutFile),
+  format: 'umd',
+  name: 'ngxOfficeUiFabric',
+  external: Object.keys(rollupGlobals),
+  output: {
+    file: join(path.dist.bundle, `${(require(join(path.tmp.src, 'tsconfig.esm5.json'))).angularCompilerOptions.flatModuleId}.umd.js`),
+  },
+  globals: rollupGlobals,
+};
+
 task('bundle:lib', runSequence(
   clean('dist', path.dist.root),
   copy('metadata', ['**/*.metadata.json'], { path: path.tmp.esm2015 }, path.dist.root),
-  copy('type-definitions:lib', ['**/*.d.ts'], { path: path.tmp.esm2015 }, path.dist.root),
-  copy('esm5', ['**/*.js', '**/*.js.map'], { path: path.tmp.esm5 }, path.dist.esm5),
-  copy('esm2015', ['**/*.js', '**/*.js.map'], { path: path.tmp.esm2015 }, path.dist.esm2015),
+  copy('type-definitions:lib', ['*/**/*.d.ts'], { path: path.tmp.esm2015 }, path.dist.src),
+  copy('type-definitions:lib', ['*.d.ts'], { path: path.tmp.esm2015 }, path.dist.root),
+  copy('package.json', ['package.json'], { path: path.tmp.src }, path.dist.root),
+  rollup('esm5', esm5RollupConfig),
+  rollup('esm2015', esm2015RollupConfig),
+  rollup('umd', umdRollupConfig),
+  uglifyJsFile('umd', path.dist.bundle, path.dist.bundle),
 ));

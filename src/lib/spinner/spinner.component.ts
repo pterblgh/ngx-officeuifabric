aafedcc27afb729c980461453c5794d7cddd1d3b
pterblgh <pterblgh@gmail.com>
2018-03-15 21:55:38 +0100

    feat(spinner): create dedicated module and component

---
+++ src/lib/spinner/spinner.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fab-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss']
+})
+export class FabricSpinnerComponent implements OnInit {
+
+  ngOnInit(): void { }
+
+}

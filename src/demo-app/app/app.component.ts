0de9a4df5e021042eeb79f06e855b48fdca8b3b2
pterblgh <pterblgh@gmail.com>
2018-03-15 19:43:48 +0100

    feat(demo-app): convert demo-app into an Angular CLI application

--- src/demo-app/app/app.component.ts
+++ src/demo-app/app/app.component.ts
@@ -9,9 +9,9 @@ import {
 } from 'ngx-officeuifabric';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 

96a89b75b1803a01e1b8c2084277427f3485671b
pterblgh <pterblgh@gmail.com>
2018-04-07 13:23:49 +0200

    feat(dialog): implement opening ComponentType<T> components

---
+++ src/lib/dialog/dialog-config.interface.ts
@@ -0,0 +1,4 @@
+export interface FabricDialogConfig {
+  canDismiss: boolean;
+  backdropClass: string;
+}
--- src/lib/dialog/dialog.service.ts
+++ src/lib/dialog/dialog.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@angular/core';
+import { ComponentType, ComponentPortal } from '@angular/cdk/portal';
+import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
+import { FabricDialogConfig } from './dialog-config.interface';
 
 @Injectable()
-export class FabricDialogService { }
+export class FabricDialogService {
+
+  private _defaultOverlayConfig: OverlayConfig;
+  private _overlayRef: OverlayRef;
+
+  constructor(private _overlay: Overlay) {
+    this._defaultOverlayConfig = {
+      positionStrategy: _overlay.position().global().centerHorizontally().centerVertically(),
+      scrollStrategy: _overlay.scrollStrategies.block(),
+      hasBackdrop: true,
+      backdropClass: 'fab-dialog-overlay-backdrop',
+    };
+  }
+
+  open<T>(component: ComponentType<T>, config?: FabricDialogConfig) {
+    if (!this._overlayRef) {
+      this._overlayRef = this._createOverlay(this._createConfig(config));
+      this._overlayRef.backdropClick().subscribe(() => this._overlayRef.detach());
+    }
+    const portal = new ComponentPortal(component);
+    this._overlayRef.attach(portal);
+  }
+
+  private _createOverlay(config: OverlayConfig) {
+    return this._overlay.create(config);
+  }
+
+  private _createConfig(config: FabricDialogConfig | undefined): OverlayConfig {
+    if (config) {
+      return new OverlayConfig({
+        ...this._defaultOverlayConfig,
+        hasBackdrop: config.canDismiss,
+        backdropClass: config.backdropClass,
+      });
+    } else {
+      return new OverlayConfig(this._defaultOverlayConfig);
+    }
+  }
+
+}

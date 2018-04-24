ecb4420c6ddebf625e324230009d891d2a0d1fe1
pterblgh <pterblgh@gmail.com>
2018-04-24 14:48:49 +0200

    style: use proper indenting

--- src/demo-app/app/app.component.ts
+++ src/demo-app/app/app.component.ts
@@ -58,19 +58,19 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.selectInputs = [
-      {key: 'Header', text: 'Actions', itemType: DropdownItemType.Header},
-      {key: 'A', text: 'Option a'},
-      {key: 'B', text: 'Option b'},
-      {key: 'C', text: 'Option c'},
-      {key: 'D', text: 'Option d'},
-      {key: 'E', text: 'Option e'},
-      {key: 'divider_2', text: '-', itemType: DropdownItemType.Divider},
-      {key: 'Header2', text: 'People', itemType: DropdownItemType.Header},
-      {key: 'F', text: 'Option f'},
-      {key: 'G', text: 'Option g'},
-      {key: 'H', text: 'Option h'},
-      {key: 'I', text: 'Option i'},
-      {key: 'J', text: 'Option j'},
+      { key: 'Header', text: 'Actions', itemType: DropdownItemType.Header },
+      { key: 'A', text: 'Option a' },
+      { key: 'B', text: 'Option b' },
+      { key: 'C', text: 'Option c' },
+      { key: 'D', text: 'Option d' },
+      { key: 'E', text: 'Option e' },
+      { key: 'divider_2', text: '-', itemType: DropdownItemType.Divider },
+      { key: 'Header2', text: 'People', itemType: DropdownItemType.Header },
+      { key: 'F', text: 'Option f' },
+      { key: 'G', text: 'Option g' },
+      { key: 'H', text: 'Option h' },
+      { key: 'I', text: 'Option i' },
+      { key: 'J', text: 'Option j' },
     ];
   }
 
